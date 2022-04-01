f5cd120bcb71757d2e62d7885954459b88a4e04f
Luis Quevedo <qfluis@gmail.com>
2022-04-01 15:44:54 +0200

    2.1 N1E1 update

--- tmp.sql
+++
@@ -1,7 +0,0 @@
-/*
--- Query: SELECT * FROM `culo-de-botella`.marcas
-LIMIT 0, 1000
-
--- Date: 2022-04-01 13:51
-*/
-INSERT INTO `` (`idmarca`,`nombre`,`proveedores_idproveedor`) VALUES (4,'prueba',1);
